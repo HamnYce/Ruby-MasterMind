da499be7028410d2a53119199db23668352ebaf6
HamnYce <hamadboa@gmail.com>
2021-10-27 01:05:17 +0300

    first commit, add beta board

--- board.rb
+++ board.rb
@@ -1,4 +1,63 @@
 # frozen_string_literal: true
 
+# TODO: Documentation Here
 class Board
+  attr_accessor :guess, :password
+
+  def initialize
+    super
+    @guess = []
+    @password = random_password
+    print_password
+  end
+
+  public
+
+  def same?
+    @password == @guess
+  end
+
+  def check_position_and_color
+    reds = []
+    (0..3).each do |i|
+      reds += ['reds'] if @password[i] == @guess[i]
+    end
+    reds
+  end
+
+  def check_color_without_position
+    whites = []
+    (0..3).each do |i|
+      @password.map { |x| x != @password[1] }
+    end
+  end
+
+  def random_password
+    [(rand * 6).to_i] + [(rand * 6).to_i] + [(rand * 6).to_i] +
+      [(rand * 6).to_i]
+  end
+
+  def take_user_guess
+    @guess = []
+    (1..4).each do |i|
+      puts "please input guess #{i}"
+      @guess += [gets.chomp.to_i]
+    end
+  end
+
+  def print_password
+    p "password #{@password}"
+  end
+
+  def to_color(num)
+    case num + 1
+    when 1 then 'red'
+    when 2 then 'green'
+    when 3 then 'yellow'
+    when 4 then 'blue'
+    when 5 then 'purple'
+    when 6 then 'teal'
+    else 'white'
+    end
+  end
 end
--- main.rb
+++ main.rb
@@ -1,1 +1,29 @@
 # frozen_string_literal: true
+
+require_relative 'board'
+
+
+guess = [1, 1, 1, 1]
+password = [1, 2, 3, 4]
+whites = []
+
+(0..3).reverse_each do |i|
+  (0..3).each do |j|
+    next if i == j
+    next unless guess[i] == password[j]
+
+    guess.delete_at(i)
+    password.delete_at(j)
+    whites << 'white'
+    next
+  end
+end
+
+p whites
+
+# b = Board.new
+
+# until b.same?
+#   b.take_user_guess
+#   p b.check_position_and_color
+# end
