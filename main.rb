77c82aa5cd264f792d0744e53fcf0bdbf0761254
HamnYce <hamadboa@gmail.com>
2021-11-19 03:16:39 +0300

    remove comment

--- main.rb
+++ main.rb
@@ -7,9 +7,3 @@ game = Driver.new
 p game.board.password
 
 game.game_loop
-
-
-# TODO: decide on code win condition,
-#         create loop that runs for either (12 times || (condition))
-#         win condition: 4 reds in array, boolean released from
-#         take_in_reds_whites
